a918fd677090ec98e178ac49526a952b4806b44b
Jofairden <jofairden@gmail.com>
2019-01-06 16:21:37 +0100

    Fix JAR building
    Can now build a FAT jar

--- src/main/kotlin/com/tmodloader/modgen/KotlinApplication.kt
+++ src/main/kotlin/com/tmodloader/modgen/KotlinApplication.kt
@@ -3,12 +3,17 @@ package com.tmodloader.modgen
 import com.tmodloader.modgen.ext.Spring
 import com.tmodloader.modgen.model.AppPreference
 import com.tmodloader.modgen.view.RootView
+import javafx.application.Application
 import javafx.scene.image.Image
 import javafx.stage.Stage
+import org.springframework.boot.Banner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationContext
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.context.annotation.ComponentScan
 import tornadofx.App
 import tornadofx.DIContainer
 import tornadofx.FX
@@ -21,16 +26,22 @@ class KotlinApplication : App(RootView::class) {
     override val primaryView = RootView::class
 
     companion object {
-        var applicationContext: ApplicationContext by singleAssign()
+        var applicationContext: ConfigurableApplicationContext by singleAssign()
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(KotlinApplication::class.java, *args)
+        }
     }
 
     override fun init() {
         super.init()
 
-        val spring = SpringApplication(KotlinApplication::class.java)
-        spring.webApplicationType = WebApplicationType.NONE
-        applicationContext = spring.run()
-
+        applicationContext = runApplication<KotlinApplication> {
+            setBannerMode(Banner.Mode.OFF)
+            webApplicationType = WebApplicationType.NONE
+        }
+        applicationContext.autowireCapableBeanFactory.autowireBean(this)
         /*
             The following code facilitates DI by Spring
          */
